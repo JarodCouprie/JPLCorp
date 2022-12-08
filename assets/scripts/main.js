6a0fd15e2b03861004c5de6db7f18070688a2b35
Spidw3b <jarod64@hotmail.fr>
2022-12-08 22:35:55 +0100

    style dark mode

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,15 +9,15 @@ let modeImg = document.querySelector(".mode-img");
 let modeText = document.querySelector(".mode-text");
 
 document.querySelector(".mode").onclick = function() {
-    if (modeText.innerText === "Dark Mode") {
-        modeText.innerText = "Light Mode";
-        modeImg.src = "./assets/logo/moon.png";
+    if (modeText.innerText === "Dark Theme") {
+        modeText.innerText = "Light Theme";
+        modeImg.src = "./assets/logo/sun-white.png";
         this.classList.toggle("dark-mode-button");
         this.classList.toggle("light-mode-button");
     }
     else {
-        modeText.innerText = "Dark Mode";
-        modeImg.src = "./assets/logo/sun.png";
+        modeText.innerText = "Dark Theme";
+        modeImg.src = "./assets/logo/moon-white.png";
         this.classList.toggle("light-mode-button");
         this.classList.toggle("dark-mode-button");
     }
